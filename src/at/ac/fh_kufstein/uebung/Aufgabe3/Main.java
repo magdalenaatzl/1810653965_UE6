c493e68d109a1b2be58d257b6753ad9ae34ba28f
magdalenaatzl <1810653965@stud.fh-kufstein.ac.at>
2018-12-19 10:57:43 +0100

    Aufgabe 3

---
+++ src/at/ac/fh_kufstein/uebung/Aufgabe3/Main.java
@@ -0,0 +1,13 @@
+package at.ac.fh_kufstein.uebung.Aufgabe3;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] array = {1,2,3,4,5,6,7,8,9,10};
+
+
+
+
+    }
+}
